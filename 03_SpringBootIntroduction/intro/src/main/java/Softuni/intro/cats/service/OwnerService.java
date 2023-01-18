5adfbb4e3ec728c101fff6839619a3ab31a18a56
Diana Angelova <diana.anguelova@gmail.com>
2023-01-18 02:29:46 +0200

    Create intro

---
+++ 03_SpringBootIntroduction/intro/src/main/java/Softuni/intro/cats/service/OwnerService.java
@@ -0,0 +1,9 @@
+package Softuni.intro.cats.service;
+
+import Softuni.intro.cats.model.dto.CreateOwnerDTO;
+
+public interface OwnerService {
+
+    void createOwner(CreateOwnerDTO createOwnerDTO);
+
+}
---
+++ 03_SpringBootIntroduction/intro/src/main/java/Softuni/intro/cats/service/impl/CatServiceImpl.java
@@ -0,0 +1,7 @@
+package Softuni.intro.cats.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CatServiceImpl {
+}
