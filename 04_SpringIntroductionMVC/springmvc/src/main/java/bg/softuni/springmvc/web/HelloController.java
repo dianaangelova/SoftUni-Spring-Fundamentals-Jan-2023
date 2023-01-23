3c25c61ee4e1c34b846da7a920d87e37d7256b89
Diana Angelova <diana.anguelova@gmail.com>
2023-01-23 11:42:19 +0200

    Create Spring MVC

--- 04_SpringIntroductionMVC/springmvc/src/main/java/bg/softuni/springmvc/web/HelloController.java
+++ 04_SpringIntroductionMVC/springmvc/src/main/java/bg/softuni/springmvc/web/HelloController.java
@@ -3,9 +3,7 @@ package bg.softuni.springmvc.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 //@RestController("/hello")
--- 04_SpringIntroductionMVC/springmvc/src/main/java/bg/softuni/springmvc/web/UserController.java
+++ 04_SpringIntroductionMVC/springmvc/src/main/java/bg/softuni/springmvc/web/UserController.java
@@ -2,11 +2,9 @@ package bg.softuni.springmvc.web;
 
 import bg.softuni.springmvc.model.UserDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user") //1. Приема get и post заявки към end point-a, kойто е user
