150d534a89e35e783b66f9ffbfa616328eb8c2cb
Diana Angelova <diana.anguelova@gmail.com>
2023-02-16 23:00:44 +0200

    Create Like Book

--- ExamPrep/likebook/src/main/java/bg/softuni/likebook/LikeBookApplication.java
+++ ExamPrep/likebook/src/main/java/bg/softuni/likebook/LikeBookApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LikeBookApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LikeBookApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LikeBookApplication.class, args);
+    }
 }
