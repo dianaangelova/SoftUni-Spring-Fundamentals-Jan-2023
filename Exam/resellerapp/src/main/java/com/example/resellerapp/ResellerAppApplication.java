6fc548defe61856d0a0a3ceadd738de677508123
Diana Angelova <diana.anguelova@gmail.com>
2023-02-18 18:59:07 +0200

    Exam 18.02.2023

---
+++ Exam/resellerapp/src/main/java/com/example/resellerapp/ResellerAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.resellerapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResellerAppApplication.class, args);
+    }
+
+}
